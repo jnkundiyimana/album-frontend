6c90b4f6627fd214c1e52fc9377f955d62eaf7dd
antesss21 <antesusic21@gmail.com>
2022-05-25 17:36:16 -0500

    Removed the bootstrap dependency

--- src/main.js
+++ src/main.js
@@ -3,8 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import router from './router'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 loadFonts()
 
