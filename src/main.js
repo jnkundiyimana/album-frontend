50e8d6b7ae83ea67666603f16d842256a0ed7c88
antesss21 <antesusic21@gmail.com>
2022-06-11 13:10:39 -0500

    Revert "Removed the bootstrap dependency"
    This reverts commit 6c90b4f6627fd214c1e52fc9377f955d62eaf7dd.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import router from './router'
-
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 loadFonts()
 
